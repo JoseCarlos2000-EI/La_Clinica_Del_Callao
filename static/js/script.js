fa383060de00fda06580057ecb221d67f5be905f
Jose Carlos <josecarlossneider@gmail.com>
2022-01-08 13:46:06 -0500

    agregar section marcas y responsive

--- static/js/script.js
+++ static/js/script.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
       
       });  
 
-      $('.carrousel-logoe').slick({
+      $('.carrousel-logo').slick({
         slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
@@ -60,7 +60,44 @@ $(document).ready(function() {
         speed:500,
         infinite: true,
         prevArrow: false,
-        nextArrow: false
+        nextArrow: false,
+        responsive: [
+          {
+            breakpoint: 1280,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+            }
+          }, 
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 320,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          }
+        ]
       }); 
 });
 
